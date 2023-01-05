1b0a3b74e31eeb016dc09dfa29e63d0078fd2cda
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2023-01-05 15:49:03 +0700

    Update key_gen.sh

--- key_gen.sh
+++ key_gen.sh
@@ -6,7 +6,7 @@ ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
 
 # LOOPING THROUGH AND DISTRIBUTING THE KEY
 
-for val in controller host1 host2 host3; do 
+for val in controller k8s-master k8s-worker1 k8s-worker2; do 
 	echo "-------------------- COPYING KEY TO ${val^^} NODE ------------------------------"
 	sshpass -p 'vagrant' ssh-copy-id -o "StrictHostKeyChecking=no" vagrant@$val 
 done
@@ -20,7 +20,7 @@ cd $PROJECT_DIRECTORY
 
 # Creating the inventory file for all 4 nodes to run some adhoc command.
 
-echo -e "controller\n\n[k8s-master]\host1\n\n[k8s-worker]\host2\nhost3" > inventory
+echo -e "controller\n\n[master]\k8s-master1\n\n[worker]\k8s-worker1\nk8s-worker2" > inventory
 echo -e "[defaults]\ninventory = inventory" > ansible.cfg
 echo -e "-------------------- RUNNING ANSBILE ADHOC COMMAND - UPTIME ------------------------------"
 echo
